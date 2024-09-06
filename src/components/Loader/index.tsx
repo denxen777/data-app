67853f623f37755ffdaf8f08d530a8c78f5b39ab
denxen777 <denxen777@gmail.com>
2024-09-06 15:12:23 +0700

    update loader

--- src/components/Loader/index.tsx
+++ src/components/Loader/index.tsx
@@ -1,10 +1,14 @@
-import { CircularProgress } from '@mui/material';
+import { Backdrop, CircularProgress } from '@mui/material';
+import { FC } from 'react';
+import { LoaderProps } from './types';
 
-export const Loader = () => {
+export const Loader: FC<LoaderProps> = (props) => {
   return (
-    <CircularProgress
-      color='success'
-      sx={{ position: 'absolute', top: '50%', left: '50%' }}
-    />
+    <Backdrop
+      sx={(theme) => ({ color: '#fff', zIndex: theme.zIndex.drawer + 1 })}
+      open={props.open}
+    >
+      <CircularProgress color='inherit' />
+    </Backdrop>
   );
 };
